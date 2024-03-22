02bf4a24af15fe8f79b810e5ffa56a0b4937a83f
paradimes <test@test.com>
2024-03-22 13:35:00 -0400

    trying to load it inside of a component now

---
+++ client/src/components/ServicesHero.tsx
@@ -0,0 +1,24 @@
+import jigsaw from "../assets/design/jigsaw.svg";
+
+export default function ServicesHero() {
+  return (
+    <div
+      id="hero"
+      className="relative  bg-center h-[40vh] mt-10"
+      style={{
+        backgroundImage: `url(${jigsaw})`,
+        backgroundAttachment: "fixed",
+      }}
+    >
+      <div className="absolute inset-0 flex items-center justify-center">
+        <h3 className="mt-6 text-4xl md:text-5xl  text-center max-w-[40vw]  shadow-inner shadow-neutral-700 px-8 py-4 bg-black rounded-3xl font-medium">
+          Take the{" "}
+          <span className="italic font-semibold bg-gradient-to-r from-orange-400 to-red-600 hover:from-[#7525ff] hover:to-[#ff00d0] inline-block text-transparent bg-clip-text">
+            first
+          </span>{" "}
+          step
+        </h3>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/ServicesPage.tsx
+++ client/src/pages/ServicesPage.tsx
@@ -1,8 +1,8 @@
 import NavBar from "../components/NavBar";
 import { FaCamera, FaImage, FaVideo } from "react-icons/fa";
 import ServicesCard from "../components/ServicesCard";
-import jigsaw from "../assets/jigsaw.svg";
 import { MdOutlineDashboardCustomize } from "react-icons/md";
+import ServicesHero from "@/components/ServicesHero";
 
 export default function ServicesPage() {
   return (
@@ -18,24 +18,7 @@ export default function ServicesPage() {
         <div id="body" className="mt-10 w-full">
           {/* Hero Section */}
           {/* BG holder and relative positiong context for its children */}
-          <div
-            id="hero"
-            className="relative  bg-center h-[40vh] mt-10"
-            style={{
-              backgroundImage: `url(${jigsaw})`,
-              backgroundAttachment: "fixed",
-            }}
-          >
-            <div className="absolute inset-0 flex items-center justify-center">
-              <h3 className="mt-6 text-4xl md:text-5xl  text-center max-w-[40vw]  shadow-inner shadow-neutral-700 px-8 py-4 bg-black rounded-3xl font-medium">
-                Take the{" "}
-                <span className="italic font-semibold bg-gradient-to-r from-orange-400 to-red-600 hover:from-[#7525ff] hover:to-[#ff00d0] inline-block text-transparent bg-clip-text">
-                  first
-                </span>{" "}
-                step
-              </h3>
-            </div>
-          </div>
+          <ServicesHero />
 
           <div className="mt-10 flex justify-center ">
             <div className="p-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8">
